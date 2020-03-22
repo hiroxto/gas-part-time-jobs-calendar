ad1461ed967961fa5ff527bc3c2d1977c22340ff
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-03-22 17:58:08 +0900

    カレンダー関連の型を追加

--- types/calendar.d.ts
+++ types/calendar.d.ts
@@ -2,3 +2,25 @@ export interface CalendarEventOptions {
   description: string;
   location: string;
 }
+
+interface EventSetting {
+  status: string;
+  id: string;
+  useDefaultTitle: boolean;
+  customTitle: string;
+  eventStartDateTime: Date;
+  eventEndDateTime: Date;
+  useDefaultLocation: boolean;
+  customLocation: string;
+  baseDescription: string;
+}
+
+interface EventsRegisterOption {
+  executeStatusValue: string;
+  addedStatusValue: string;
+  calendarId: string;
+  calendarSheetName: string;
+  defaultTitle: string;
+  defaultLocation: string;
+  popupMinutes: string;
+}
