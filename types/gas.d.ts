ae2eff28f6ec790ccf0df695b67eec529f0be437
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-03-23 02:59:16 +0900

    Google Apps Script の型を gas.d.ts に追加

---
+++ types/gas.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+import Calendar = GoogleAppsScript.Calendar.Calendar;
+import CalendarEvent = GoogleAppsScript.Calendar.CalendarEvent;
+import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+import Task = GoogleAppsScript.Tasks.Schema.Task;
+
+export {
+  CalendarEvent,
+  Sheet,
+  Calendar,
+  Task,
+};
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,3 @@
 export * from './calendar';
 export * from './task';
+export * from './gas';
