f249e0435693f1960c8437aae30d4232237faa38
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-03-30 14:32:30 +0900

    ESLint のルールに @typescript-eslint/array-type と @typescript-eslint/consistent-type-definitions を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,7 @@ module.exports = {
     'no-new': 'off',
     indent: ['error', 2],
     'promise/catch-or-return': ['error', { allowThen: true, terminationMethod: ['catch', 'asCallback', 'finally'] }],
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
   },
 };
