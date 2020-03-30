64521c8417b97e099fdd23b7edf99c3d86cda0fa
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-03-30 14:21:28 +0900

    @typescript-eslint/array-type を削除
    デフォルトオプションが array なので指定する必要がないため

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
     'no-new': 'off',
     indent: ['error', 2],
     'promise/catch-or-return': ['error', { allowThen: true, terminationMethod: ['catch', 'asCallback', 'finally'] }],
-    '@typescript-eslint/array-type': ['error', { default: 'array' }],
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
     '@typescript-eslint/no-use-before-define': 'off',
   },
