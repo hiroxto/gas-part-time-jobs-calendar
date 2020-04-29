0a68503041f7a6e88d536f946c0cc813fa97cc6c
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-04-29 15:01:38 +0900

    Application.start() メソッドを作成

--- src/application.ts
+++ src/application.ts
@@ -1,4 +1,5 @@
 import { ApplicationOptions } from '~/types/application';
+import { Sheet } from '~/types';
 
 export class Application {
   options: ApplicationOptions;
@@ -8,5 +9,24 @@ export class Application {
    */
   constructor (options: ApplicationOptions) {
     this.options = options;
+  }
+
+  /**
+   * 登録を開始する
+   */
+  start (): void {
+    const sheet = this.getSheet();
+
+    for (let rowNumber = 2; rowNumber <= sheet.getLastRow(); rowNumber++) {
+    }
+  }
+
+  /**
+   * スプレッドシートを取得する
+   *
+   * @returns イベントの設定が入ったシート
+   */
+  protected getSheet (): Sheet {
+    return SpreadsheetApp.getActiveSpreadsheet().getSheetByName(this.options.sheetName);
   }
 }
